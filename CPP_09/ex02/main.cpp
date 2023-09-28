8eb936fa7dbb87e8141fe2a97288d031accb81c7
Brahim Chifour <bchifour@c1r7p2.1337.ma>
2023-09-28 12:08:21 +0100

    hi

--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bchifour <bchifour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 10:53:32 by bchifour          #+#    #+#             */
-/*   Updated: 2023/09/26 18:39:24 by bchifour         ###   ########.fr       */
+/*   Updated: 2023/09/28 12:05:02 by bchifour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,10 @@
 #include <sstream>
 #include <unistd.h>
 
-
-// bool vectorCompare(const std::vector<int>& a, const std::vector<int>& b) {
-//     // You can define your custom comparison logic here.
-//     // For example, you may want to compare the sum of elements in the vectors.
-//     return a.end()-1 < b.end()-1;
-// }
+int count = 0;
 
 bool vectorCompare(const std::vector<int>& a, const std::vector<int>& b) {
-    // You can define your custom comparison logic here.
-    // For example, you may want to compare the sum of elements in the vectors.
-    std::cout << (a.back()) << " "  <<  (b.back()) << std::endl;
+    count++;
     return a.back() < b.back();
 }
 
@@ -45,8 +38,6 @@ void split_each_peers(std::vector<std::vector<int> > &peers)
         it->erase(_it + i, _it + j);
         peers.insert(it+1, tmp);
         it++;
-        // it->insert(it->begin(), tmp.begin(), tmp.end());
-         std::cout << tmp.size() << std::endl;
         tmp.clear();
     }
     
@@ -54,7 +45,7 @@ void split_each_peers(std::vector<std::vector<int> > &peers)
 
 void print_doublevector(std::vector<std::vector<int> > doubleVector)
 {
-    std::cout << "print double vector" << std::endl;
+    // std::cout << "print double vector" << std::endl;
     std::vector<std::vector<int> >::iterator it = doubleVector.begin();
     std::vector<int>::iterator _it = it->begin();
     for(it = doubleVector.begin(); it != doubleVector.end(); it++)
@@ -65,15 +56,37 @@ void print_doublevector(std::vector<std::vector<int> > doubleVector)
             std::cout << *_it << " ";
             _it++;
         }
-        std::cout <<  std::endl;
+        std::cout << "   ";
     }
+    std::cout << std::endl;
+}
+
+int jacobsthal(int index)
+{
+    if (index == 0)
+        return 0;
+    if (index == 1)
+        return 1;
+    return jacobsthal(index - 1) + 2 * jacobsthal(index - 2);
+}
+std::vector<int> build_jacob_insertion_sequence(std::vector<std::vector<int> > pand)
+{
+    int len = pand.size();
+    std::vector<int> jacob;
+    int jacob_len = 3;
+    while(jacobsthal(jacob_len) < len)
+    {
+        jacob.push_back(jacobsthal(jacob_len));
+        jacob_len++;
+    }
+    return jacob;
+    
 }
 
 void merge_insertion(std::vector<std::vector<int> > &peers)
 {
     std::vector<std::vector<int> >::iterator it = peers.begin();
     std::vector<int>::iterator __it;
-    // __it--;
     int len = peers.size() % 2 != 0;
     std::vector<int> rest;
     if (len)
@@ -86,7 +99,6 @@ void merge_insertion(std::vector<std::vector<int> > &peers)
         rest.push_back(-1);
         
     }
-    std::cout << "-----------------------" << std::endl;
     if (peers.size()  >= 2)
     {
         for(it = peers.begin(); it != peers.end(); it++)
@@ -95,49 +107,24 @@ void merge_insertion(std::vector<std::vector<int> > &peers)
             
             if (it+1 != peers.end())
             {
-                 std::cout << "----------- " << *(it->end() - 1)  << " " <<*((it + 1)->end() - 1) << std::endl;
+                count++;
                 if (*(it->end() - 1) > *((it + 1)->end() - 1))
                 {
-                //     it->p(it->begin(), ((it + 1));
                 it->insert(it->begin(), (it+1)->begin(), (it+1)->end());
-                // it->erase(it->begin(), it->begin() + (it+1)->size());
                     
                 }
                 else
                 {
                     it->insert(it->end(), (it+1)->begin(), (it+1)->end());
-                    // it->erase((it+1)->begin(), (it+1)->end()-1);
                 }
-                // it++;
                 (it+1)->erase((it+1)->begin(), (it+1)->end());
                 peers.erase(it+1);
-                // (*(it + 1)).pop_back();
-                // std::cout << *((it + 1)->end() - 1) << std::endl;
             }
-            // if (*__it > *(__it + 1))
-            // {
-                
-            // }
-            // __it++;
         
         }
-        // std::cout << peers.size() << std::endl;
-        print_doublevector(peers);
-        // pause();
         if (peers.size() >= 2)
             merge_insertion(peers);
     }
-    // std::cout << "------------pause-----------" << std::endl;
-    // std::cout << peers.size() << std::endl;
-    // std::cout << "-----------------------" << std::endl;
-    // int size = peers.size() / 2;
-    // std::vector<int> tmp;
-    // for( int size = __it->size() / 2; size < peers.size(); size++)
-    // {
-    //     tmp.push_back(*((peers.at(size)).begin()));
-    // }
-    // peers.push_back(tmp);
-    std::cout << "-----------split_each_peers------------" << std::endl;
     split_each_peers(peers);
     if (peers.size() > 2)
     {
@@ -149,101 +136,138 @@ void merge_insertion(std::vector<std::vector<int> > &peers)
             
             pand.push_back(*it);
             peers.erase(it);
-            // it++;
-            // main_chain.push_back(*it);
         }
         if (rest.at(0) != -1)
             pand.push_back(rest);
         
-        
-        for(it = pand.begin(); it != pand.end(); it++)
+        it = pand.begin();
+        // for(it = pand.begin(); it != pand.end(); it++)
+        // {
+        //     std::vector<int>::iterator _it = it->begin();
+        //     std::vector<int> jacob = build_jacob_insertion_sequence(pand);
+        //     std::vector<int>::iterator __it = jacob.begin();
+        //     std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin(), peers.end(), *(it), vectorCompare);
+        //     peers.insert(customUpperBound, *(it));
+        // }
+        // std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin(), peers.begin()+1, *(it), vectorCompare);
+        peers.insert(peers.begin(), *(it));
+        std::vector<std::vector<int> >::iterator  customUpperBound = std::upper_bound(peers.begin(), peers.begin()+2, *(it+1), vectorCompare);
+        peers.insert(customUpperBound, *(it+1));
+        std::vector<int> jacob = build_jacob_insertion_sequence(pand);
+        std::vector<int>::iterator __it = jacob.begin();
+        if(jacob.size() ==  0)
         {
-            std::vector<int>::iterator _it = it->begin();
-            while(_it != it->end())
-            {
-                std::cout << *_it << " - ";
-                _it++;
-            }
-            std::cout <<  std::endl;
-        std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin(), peers.end(), *it, vectorCompare);
-        // std::cout << "-------------*----------" << *customUpperBound << std::endl;
-    //    while(_it != it->end())
-    //     {
-    //         std::cout << *_it  << std::endl;
-    //         _it++;
-    //     }
-        print_doublevector(peers);
-        peers.insert(customUpperBound, *(it));
-        print_doublevector(peers);
+            customUpperBound = std::upper_bound(peers.begin(), peers.begin()+4, *(it+2), vectorCompare);
+            peers.insert(customUpperBound, *(it+2));
         }
+    
         
+        if (jacob.size() > 0 )
+        {
+            int index = 8;
+            int zip = 0;
+            for(__it = jacob.begin(); __it != jacob.end(); __it++)
+            {
+                int i = *__it;
+                int j;
+                if (__it == jacob.begin())
+                    j = 1;
+                else
+                    j = *(__it - 1);
+                while(i > j)
+                {
+                    std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin(), peers.begin() + index-1, *(it + i), vectorCompare);
+                    peers.insert(customUpperBound, *(it + i));
+                    index += 2;
+                    // zip+=2;
+                    i--;
+                }
+            }
+            if (*(jacob.end() -1) < pand.size())
+            {
+                int i = pand.size() - 1;
+                int j = *(jacob.end() -1);
+                // int index = ;
+                while(i > j)
+                {
+                    if (i == pand.size() - 1)
+                    {
+                        std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin(), peers.end(), *(it + i), vectorCompare);
+                        peers.insert(customUpperBound, *(it + i));
+                    }
+                    else
+                    {
+                        // std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin() + (index - 10), peers.begin() + index -1, *(it + i), vectorCompare);
+                        std::vector<std::vector<int> >::iterator customUpperBound = std::upper_bound(peers.begin() , peers.begin() + (index), *(it + i), vectorCompare);
+
+                        peers.insert(customUpperBound, *(it + i));
+                        if (i % 2 != 0)
+                            index -= 1;
+                    }
+                    i--;
+                }
+            }
+        } 
     }
-    
-    
-    // pause();
-    
-    // insertion_sort
-   
-    
 }
 
-std::vector<std::vector<int> > creat_peers(std::vector<int> vc)
-{
-    std::vector<std::vector<int> > doubleVector;
-    std::vector<int>::iterator it = vc.begin();
-    std::vector<std::vector<int> >::iterator iit = doubleVector.begin();
-    std::vector<int> peers;
-    int i = 0;
-    int len = vc.size() % 2 != 0;
-    int rest;
-    if (len)
-    {
-        rest = vc.at(vc.size() - 1);
-        vc.pop_back();
-    }
-    else
-        rest = -1;
-    while(it != vc.end())
-    {
-        std::vector<int> tmp;
-        i = 0;
-        while(i < 1 && it != vc.end())
-        {
-            tmp.push_back(*it);
-            it++;
-            if (it != vc.end())
-            {
-                tmp.push_back(*it);
-                it++;
-            }
-            i++;
-        }
-        doubleVector.push_back(tmp);
-        tmp.clear();
-    }
-    return doubleVector;
-}
-void sort_each_peer(std::vector<std::vector<int> > &peers)
-{
-    std::vector<std::vector<int> >::iterator it = peers.begin();
-    std::vector<int>::iterator _it = it->begin();
-    for(it = peers.begin(); it != peers.end(); it++)
-    {
-        _it = it->begin();
-        while(_it != it->end())
-        {
-            if (*_it > *(_it + 1))
-            {
-                int tmp = *_it;
-                *_it = *(_it + 1);
-                *(_it + 1) = tmp;
-            }
-            _it++;
-            _it++;
-        }
-    }
+// std::vector<std::vector<int> > creat_peers(std::vector<int> vc)
+// {
+//     std::vector<std::vector<int> > doubleVector;
+//     std::vector<int>::iterator it = vc.begin();
+//     std::vector<std::vector<int> >::iterator iit = doubleVector.begin();
+//     std::vector<int> peers;
+//     int i = 0;
+//     int len = vc.size() % 2 != 0;
+//     int rest;
+//     if (len)
+//     {
+//         rest = vc.at(vc.size() - 1);
+//         vc.pop_back();
+//     }
+//     else
+//         rest = -1;
+//     while(it != vc.end())
+//     {
+//         std::vector<int> tmp;
+//         i = 0;
+//         while(i < 1 && it != vc.end())
+//         {
+//             tmp.push_back(*it);
+//             it++;
+//             if (it != vc.end())
+//             {
+//                 tmp.push_back(*it);
+//                 it++;
+//             }
+//             i++;
+//         }
+//         doubleVector.push_back(tmp);
+//         tmp.clear();
+//     }
+//     return doubleVector;
+// }
+// void sort_each_peer(std::vector<std::vector<int> > &peers)
+// {
+//     std::vector<std::vector<int> >::iterator it = peers.begin();
+//     std::vector<int>::iterator _it = it->begin();
+//     for(it = peers.begin(); it != peers.end(); it++)
+//     {
+//         _it = it->begin();
+//         while(_it != it->end())
+//         {
+//             if (*_it > *(_it + 1))
+//             {
+//                 int tmp = *_it;
+//                 *_it = *(_it + 1);
+//                 *(_it + 1) = tmp;
+//             }
+//             _it++;
+//             _it++;
+//         }
+//     }
 
-}
+// }
 int main (int ac, char **av)
 {
     std::vector<std::vector<int> > doubleVector;
@@ -257,25 +281,11 @@ int main (int ac, char **av)
     {
         int i = strtod(token.c_str(), NULL);
         vc.push_back(i);
+        doubleVector.push_back(vc);
+        vc.clear();
     }
-    for(it = vc.begin(); it != vc.end(); it++)
-        std::cout << *it << " ";
-    std::cout << "\n" << std::endl;
-    
-    doubleVector = creat_peers(vc);
-    // std::cout << doubleVector.size() << "\n";
-    sort_each_peer(doubleVector);
-    for(__it = doubleVector.begin(); __it != doubleVector.end(); __it++)
-    {
-        it = __it->begin();
-        while(it != __it->end())
-        {
-            std::cout << *it << " ";
-            it++;
-        }
-        std::cout << "    ";
-    }
-    std::cout << std::endl;
     merge_insertion(doubleVector);
-    // pause();
+    print_doublevector(doubleVector);
+    
+    std::cout << "comparition " << count << std::endl;
 }
